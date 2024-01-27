bfd5e7285f09ffa502947fc5a809197a1eaeca33
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2024-01-27 14:46:40 +0700

    remove sequilzer

--- App.ts
+++ App.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-import SEQUELIZE from "./configs/sequelize";
 import UsersRoute from "./routes/Users.route";
 
 const APP = express();
@@ -8,29 +7,11 @@ const PORT = 3000;
 // MIDDLEWARE EXPRESS JSON
 APP.use(express.json());
 
-// Sync the models with the database
-// sequelize.sync();
-
-// app.get("/", async (req: Request, res: Response) => {
-//   // Create a sample user
-//   const newUser = await Users.create({
-//     name: "John Doe",
-//     email: "john@example.com",
-//     password: "Hello World",
-//   });
-
-//   // Retrieve all users from the database
-//   const users = await Users.findAll();
-
-//   res.json(users);
-// });
-
 // API USERS
 APP.use("/api/users", UsersRoute);
 
 const start = async (): Promise<void> => {
   try {
-    await SEQUELIZE.sync();
     APP.listen(3000, () => {
       console.log(`Server is running at http://localhost:${PORT}`);
     });
