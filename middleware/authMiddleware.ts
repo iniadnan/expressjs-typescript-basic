f85de4ea9a00fcd158da8f536c4445abbb4bc93a
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2024-01-29 13:51:20 +0700

    route middleware done

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,22 +1,28 @@
 import { Request, Response, NextFunction } from "express";
-import { verifyToken } from "../configs/Jwt";
+import jwt from "jsonwebtoken";
 
-export const authenticateUser = (
-  req: Request,
+// Extend the Express Request type
+interface AuthenticatedRequest extends Request {
+  user?: any; // or your user object type
+}
+
+export const authMiddleware = (
+  req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+  const token = req.header("Authorization");
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const decodedToken = verifyToken(token);
-
-  if (!decodedToken) {
-    return res.status(401).json({ message: "Unauthorized" });
+  try {
+    const decoded = jwt.verify(token, "your-secret-key");
+    req.user = decoded; // Attach user information to the request
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Invalid token" });
   }
-
-  next();
 };
--- routes/Users.route.ts
+++ routes/Users.route.ts
@@ -8,25 +8,24 @@ import {
   deleteOneUser,
   loginUser,
 } from "../controllers/Users.controller";
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const UsersRoute = express.Router();
 
+// [PUBLIC API]
 // GET: ALL USER [http://localhost:3000/api/users/all]
 UsersRoute.get("/all", showAllUser);
-
 // GET: SINGLE USER [http://localhost:3000/api/users/user/email@email.com]
 UsersRoute.get("/user/:email", showSingleUser);
-
 // CREATE: CREATE NEW USER [http://localhost:3000/api/users/create]
 UsersRoute.post("/create", createUser);
-
-// UPDATE: UPDATE ARTICLE [http://localhost:3000/api/users/update]
-UsersRoute.put("/update", updateUser);
-
-// DELETE: DELETE SINGLE USER [http://localhost:3000/api/users/single/1234]
-UsersRoute.delete("/single/:id", deleteOneUser);
-
 // AUTH: LOGIN [http://localhost:3000/api/users/login]
 UsersRoute.post("/login", loginUser);
+
+// [AUTHENTICATION API]
+// UPDATE: UPDATE ARTICLE [http://localhost:3000/api/users/update]
+UsersRoute.put("/update", authMiddleware, updateUser);
+// DELETE: DELETE SINGLE USER [http://localhost:3000/api/users/single/1234]
+UsersRoute.delete("/single/:id", authMiddleware, deleteOneUser);
 
 export default UsersRoute;
